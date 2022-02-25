f02dc7bc7872c0516148dd663c80a29df4f354ce
alissonrangel <alisson.rangel@hotmail.com>
2022-02-25 02:06:25 -0300

    adding cors in index.js with tsc -w

--- dist/index.js
+++ dist/index.js
@@ -6,11 +6,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const http_1 = __importDefault(require("http"));
 const socket_io_1 = require("socket.io");
+const cors_1 = __importDefault(require("cors"));
 const router_1 = __importDefault(require("./router"));
 const users_1 = require("./users");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
 const server = http_1.default.createServer(app);
 const io = new socket_io_1.Server(server, {
     cors: {
